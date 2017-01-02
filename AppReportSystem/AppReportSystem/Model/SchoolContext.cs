96989b3a1b4502151a7d73e379f9d54412a0b9c8
dhani1803 <dhani.s.wibowo@gmail.com>
2017-01-02 11:52:50 +0700

    added dbContext
    supaya class bisa digenerate ke database update class ini

---
+++ AppReportSystem/AppReportSystem/Model/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReportSystem.Model
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
