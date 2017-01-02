8b8ba0d497b3591274ea621827895e834cd92d39
dhani1803 <dhani.s.wibowo@gmail.com>
2017-01-02 11:52:06 +0700

    Added class student and Person

---
+++ AppReportSystem/AppReportSystem/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReportSystem.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+
+        public Person(string name, string id, DateTime birth, string gender)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.DateOfBirth = birth;
+            this.Gender = gender;
+        }
+    }
+}
---
+++ AppReportSystem/AppReportSystem/Model/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReportSystem.Model
+{
+    public class Student : Person
+    {
+
+        public string ofClass { get; set; }
+
+        public Student(string name, string id, DateTime birth, string gender, string kelas) : base(name, id, birth, gender)
+        {
+            this.ofClass = kelas;
+        }
+    }
+}
